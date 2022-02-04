8bd9091bfc989b3c2fce99c2f93982821e44f22a
jear <jerome.armand@hpe.com>
2022-02-04 16:21:50 +0100

    add clusters config

---
+++ cleanup.sh
@@ -0,0 +1,11 @@
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+docker volume rm -f $(docker volume ls)
+sudo rm -rf /run/secrets/kubernetes.io
+sudo rm -rf /var/lib/etcd
+sudo rm -rf /var/lib/kubelet
+sudo rm -rf /var/lib/rancher
+sudo rm -rf /etc/kubernetes
+sudo rm -rf /opt/rke
+sudo systemctl restart docker
+
